4fa68902a2e40b57d5f9c64186f35fac54b5cea6
Vladimir Saponko <saponko@gmail.com>
2020-10-30 12:25:57 +0300

    initial commit

---
+++ app/reflexes/counter_reflex.rb
@@ -0,0 +1,7 @@
+class CounterReflex < StimulusReflex::Reflex
+  delegate :current_user, to: :connection
+
+  def increment
+    @count = element.dataset[:count].to_i + element.dataset[:step].to_i
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+
+  resources :pages, only: :index
+
+  root 'pages#index'
+end
